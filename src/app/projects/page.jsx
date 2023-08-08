3f3d7b192a26c1d9c4d53df4457de8633a802e62
leuzaoo <leuzaoocontato@gmail.com>
2023-08-08 00:30:36 -0300

    Updated

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import ButtonLink from "../components/ButtonLink";
 import {
@@ -6,10 +7,9 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverBody,
-  PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor,Button
+  Button,
 } from "@chakra-ui/react";
 
 export default function ProjectsPage() {
@@ -17,7 +17,7 @@ export default function ProjectsPage() {
     <div className="px-8 w-full flex flex-col lg:items-center lg:mx-auto">
       <div className="mt-10 lg:mt-20 lg:max-w-2xl">
         <h2 className="font-bold text-transparent text-left text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          Portfolio Web
+          Portfolio
         </h2>
         <p className="mt-4">
           Realizei uma SPA para destacar as habilidades e realizações do
@@ -28,22 +28,24 @@ export default function ProjectsPage() {
           <a href="https://yhwconsulting.netlify.app/" target="_blank">
             <ButtonLink text="Ver Projeto" />
           </a>
-          <Popover isLazy>
-            <PopoverTrigger>
-              <Button>Click me</Button>
-            </PopoverTrigger>
-            <PopoverContent>
-              <PopoverHeader fontWeight="semibold">
-                Popover placement
-              </PopoverHeader>
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <PopoverBody>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore.
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
+          <div>
+            <Popover isLazy>
+              <PopoverTrigger>
+                <Button className="bg-gradient-to-r from-blue-700 to-blue-500 px-5 py-3 text-sm rounded-lg">
+                  Tecnologias
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="right-8 bg-gradient-to-br from-gray-800 to-gray-900 border border-gray-700 text-white max-w-xs rounded-lg">
+                <PopoverHeader className="flex gap-8 justify-between px-4 py-6 text-transparent font-semibold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                  HTML & SCSS
+                  <PopoverCloseButton className="text-white" />
+                </PopoverHeader>
+                {/* <PopoverBody className="px-4 pb-2 text-blue-400 underline">
+                  Acessar Repositório
+                </PopoverBody> */}
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
       </div>
 
