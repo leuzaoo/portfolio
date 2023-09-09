90a0778be858c9dabf4707d1d3ef65d827a85bd6
leuzaoo <leuzaoocontato@gmail.com>
2023-09-09 00:19:05 -0300

    Created component to translate page

---
+++ app/components/LangSwitcher.jsx
@@ -0,0 +1,39 @@
+import us from "@/public/unitedstates-flag.svg";
+import br from "@/public/brazil-flag.svg";
+import { useTranslation } from "react-i18next";
+import Image from "next/image";
+
+const languageOption = [
+  {
+    name: "Português",
+    value: "ptBr",
+    flag: br,
+  },
+  {
+    name: "United States",
+    value: "enUS",
+    flag: us,
+  },
+];
+
+export default function LangSwitcher() {
+  const { t, i18n } = useTranslation();
+  return (
+    <div className="absolute right-10 top-24">
+      {languageOption.map((languageOption) => (
+        <button
+          key={languageOption.value}
+          onClick={() => {
+            i18n.changeLanguage(languageOption.value);
+          }}
+        >
+          <Image
+            className="w-10"
+            src={languageOption.flag}
+            alt={languageOption.name}
+          />
+        </button>
+      ))}
+    </div>
+  );
+}
