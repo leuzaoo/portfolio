b5ba5f28d6392f3cd8aa237a96706288a191392b
leuzaoo <leuzaoocontato@gmail.com>
2023-09-11 21:44:07 -0300

    Add title to app

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,17 +3,17 @@ import Navbar from "./components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/i18n";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Portolio | Leonardo Oliveira",
+  description: "Portfolio about Leonardo Oliveira.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <Head>
-        <title>Portfolio | Leonardo Oliveira</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
