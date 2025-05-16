13277109ffad6108814120227167996f4b1f20f5
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-16 09:57:45 -0300

    Project info component

---
+++ app/components/ProjectInfo.jsx
@@ -0,0 +1,25 @@
+import { useTranslation } from "react-i18next";
+
+import AboutProject from "./AboutProject";
+import ButtonLink from "./ButtonLink";
+import Popup from "./Popup";
+
+const ProjectInfo = ({ href, title, description, popUpTitle }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <AboutProject title={title} description={description} />
+      <div className="mt-4 flex items-center justify-between md:justify-normal md:gap-10">
+        {href && (
+          <a href={href} target="_blank">
+            <ButtonLink text={t("SeeProject")} />
+          </a>
+        )}
+        <Popup title={popUpTitle} />
+      </div>
+    </>
+  );
+};
+
+export default ProjectInfo;
